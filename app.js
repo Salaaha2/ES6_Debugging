1f7cfa581da23a668c38c23e9ff49b3ac6968015
alykaa <alykaasalaah@Alykaas-MBP.home>
2023-08-17 20:59:58 -0400

    new push

---
+++ app.js
@@ -0,0 +1,45 @@
+// Create two Variables that have numbers as their values
+var valueOne = 10;
+var valueTwo = 15;
+
+// Create a function that returns the sum of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments
+function sumNum(a,b) {
+    return a + b;
+}
+
+const sumNum = (a, b) => {
+    return a + b;
+}
+console.log(valueOne, valueTwo);
+
+// Create a function that returns the difference of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+function subNum(a,b) {
+    return a - b;
+}
+
+const subNum = (a, b) => {
+    return a - b;
+}
+console.log(a - b);
+
+// Create a function that returns two arguments multiplied by each other. Then console.log the function with the variables from step two as your two arguments.
+function mulNum(a, b) {
+    return a * b;
+}
+
+const mulNum = (a, b) => {
+    return a * b;
+}
+console.log(a * b);
+
+// Create a function that returns divides the first argument by the second argument and returns the new value. Then console.log the function with the variables from step two as your two arguments.
+function divNum(a, b) {
+    return a/b;
+}
+
+const divNum = (a , b) => {
+    return a/b;
+}
+console.log(a / b);
+
+// Take the four functions that you created above and turn them into Arrow Functions, do not delete the original functions made, please have the original function and the arrow function on your completed homework. You should have a total of eight functions, four regular and four arrow functions.
